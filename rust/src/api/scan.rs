6457bbefb4b670b239e96786648a8319fe32c40a
Oleksandr <alexlukianoff@gmail.com>
2026-04-19 17:43:17 +0200

    Minor fixes of edge detection

--- rust/src/api/scan.rs
+++ rust/src/api/scan.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use opencv::{core, imgcodecs, imgproc};
 
+#[derive(Clone, Copy)]
 pub struct DocPoint {
     pub x: f32,
     pub y: f32,
@@ -20,49 +21,78 @@ pub fn detect_document_edges(image_bytes: Vec<u8>) -> Result<Vec<DocPoint>> {
     imgproc::gaussian_blur_def(&gray, &mut blurred, ksize, 0.0)?;
 
     let mut edges = core::Mat::default();
-    imgproc::canny_def(&blurred, &mut edges, 75.0, 200.0)?;
+    imgproc::canny_def(&blurred, &mut edges, 50.0, 150.0)?;
 
-    let mut result_bytes = core::Vector::<u8>::new();
-    imgcodecs::imencode(".jpg", &edges, &mut result_bytes, &core::Vector::new())?;
+    let mut dilated_edges = core::Mat::default();
+    let kernel = imgproc::get_structuring_element(
+        imgproc::MORPH_RECT,
+        core::Size::new(3, 3),
+        core::Point::new(-1, -1),
+    )?;
+    imgproc::dilate_def(&edges, &mut dilated_edges, &kernel)?;
 
     let mut contours = core::Vector::<core::Vector<core::Point>>::new();
     imgproc::find_contours_def(
-        &edges,
+        &dilated_edges,
         &mut contours,
         imgproc::RETR_LIST,
         imgproc::CHAIN_APPROX_SIMPLE,
     )?;
 
-    let mut max_area = 0.0;
-    let mut best_contour = core::Vector::<core::Point>::new();
-
+    let mut contour_info: Vec<(f64, core::Vector<core::Point>)> = Vec::new();
     for i in 0..contours.len() {
         let contour = contours.get(i)?;
         let area = imgproc::contour_area(&contour, false)?;
 
-        if area > 1000.0 {
-            let perimeter = imgproc::arc_length(&contour, true)?;
+        if area > 5000.0 {
+            contour_info.push((area, contour));
+        }
+    }
 
-            let epsilon = 0.02 * perimeter;
-            let mut approx = core::Vector::<core::Point>::new();
-            imgproc::approx_poly_dp(&contour, &mut approx, epsilon, true)?;
+    contour_info.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
 
-            if approx.len() == 4 && area > max_area {
-                max_area = area;
-                best_contour = approx;
-            }
+    let mut best_contour = core::Vector::<core::Point>::new();
+
+    for (_, contour) in contour_info {
+        let perimeter = imgproc::arc_length(&contour, true)?;
+
+        let epsilon = 0.04 * perimeter;
+
+        let mut approx = core::Vector::<core::Point>::new();
+        imgproc::approx_poly_dp(&contour, &mut approx, epsilon, true)?;
+
+        if approx.len() == 4 {
+            best_contour = approx;
+            break;
         }
     }
 
     let mut result = Vec::new();
+
     if best_contour.len() == 4 {
+        let mut raw_points = Vec::new();
         for i in 0..4 {
             let pt = best_contour.get(i)?;
-            result.push(DocPoint {
+            raw_points.push(DocPoint {
                 x: pt.x as f32,
                 y: pt.y as f32,
             })
         }
+
+        let mut by_sum = raw_points.clone();
+        by_sum.sort_by(|a, b| (a.x + a.y).partial_cmp(&(b.x + b.y)).unwrap());
+        let top_left = by_sum[0];
+        let bottom_right = by_sum[3];
+
+        let mut by_diff = raw_points.clone();
+        by_diff.sort_by(|a, b| (a.y - a.x).partial_cmp(&(b.y - b.x)).unwrap());
+        let top_right = by_diff[0];
+        let bottom_left = by_diff[3];
+
+        result.push(top_left);
+        result.push(top_right);
+        result.push(bottom_right);
+        result.push(bottom_left);
     }
 
     Ok(result)
